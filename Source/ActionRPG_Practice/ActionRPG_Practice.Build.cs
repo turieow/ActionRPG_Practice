35c08d35a52a3b35e160b7b906fdf907da8758a6
aaaa <mai_19n1102996@nnn.ed.jp>
2021-07-28 16:26:02 +0900

    AbilitiySystemComponentを使うためのプラグインを追加

--- Source/ActionRPG_Practice/ActionRPG_Practice.Build.cs
+++ Source/ActionRPG_Practice/ActionRPG_Practice.Build.cs
@@ -8,6 +8,6 @@ public class ActionRPG_Practice : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "GameplayAbilities", "GameplayTasks" });
 	}
 }
