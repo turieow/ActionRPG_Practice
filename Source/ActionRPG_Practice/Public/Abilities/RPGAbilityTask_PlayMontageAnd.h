d2bbbfa29802646316cf07866ca4baef203d0192
aaaa <mai_19n1102996@nnn.ed.jp>
2021-07-28 17:16:58 +0900

    RPGAbilityTask_PlayMontageAnd 追加

---
+++ Source/ActionRPG_Practice/Public/Abilities/RPGAbilityTask_PlayMontageAnd.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "RPGAbilityTask_PlayMontageAnd.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONRPG_PRACTICE_API URPGAbilityTask_PlayMontageAnd : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+};
